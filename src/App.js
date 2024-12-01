65ebc8c92f172fb716107fe0c0cf8f9d28e64c57
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-12-01 19:06:43 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.js";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { LockClosedIcon } from "@radix-ui/react-icons";
+const Login = () => {
+  return (
+    <div>
+      <p className="common-page w-full h-full bg-background">
+        <LockClosedIcon /> Login 페이지 입니다. . .
+      </p>
+    </div>
+  );
+};
+
+export default Login;
